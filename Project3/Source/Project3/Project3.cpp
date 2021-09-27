8281ac805e49e3b72a2d7ce7cbd4284de6883f25
scRaul <78058685+Raul-Sc@users.noreply.github.com>
2021-09-26 18:35:19 -0700

    Adding Existing Project

---
+++ Project3/Source/Project3/Project3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project3, "Project3" );
