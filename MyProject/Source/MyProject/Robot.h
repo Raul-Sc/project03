d9f5440cdf1b8ba5189fd582970a007d762b2c7a
Raul Ramirez <78058685+Raul-Sc@users.noreply.github.com>
2021-10-16 13:46:49 -0700

    Starting Main player class

---
+++ MyProject/Source/MyProject/Robot.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyPawn.h"
+
+
+#include "Robot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API ARobot : public AMyPawn
+{
+	GENERATED_BODY()
+	
+};
